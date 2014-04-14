7e3af5b71852cf18366d62e8ca4b8109f444235b
Dharmit Shah <dharmit.dev@gmail.com>
2014-04-14 19:55:25 +0530

    Finished Part II - Templates.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ microblog
 =========
 
 A python flask application
+
+I am referring the [Flask Mega Tutorial by Miguel
+Grinberg](http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world).
+It's about creating a microblog like Twitter using Python Flask.
+
+Hope I finish it. :)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,24 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 @app.route('/index.html')
 @app.route('/home')
 def index():
-    return "Hello, World"
+    #return "Hello, World"
+    user = {"nickname": "Dharmit"}
+    posts = [
+            {
+                'author': {'nickname': 'Komal'},
+                'body': 'Beautiful day in Pune'
+            },
+            {
+                'author': {'nickname': 'Dharmit'},
+                'body': 'Ahmedabad is anytime better.'
+            }
+            ]
+    return render_template("index.html",
+            title = "Home",
+            user = user,
+            posts = posts)
