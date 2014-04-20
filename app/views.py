73c56b46606462806b4bad2959dcb7feb09c1824
Dharmit Shah <dharmit.dev@gmail.com>
2014-04-20 17:47:04 +0530

    Logout option added.

--- app/views.py
+++ app/views.py
@@ -44,6 +44,11 @@ def login():
                             form = form,
                             providers = app.config['OPENID_PROVIDERS'])
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
