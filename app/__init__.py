a298920e994a9af5696fa96705a00de273b547cc
Dharmit Shah <dharmit.dev@gmail.com>
2014-05-06 13:22:17 +0530

    Send mail using gmail account.

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, \
         MAIL_PASSWORD
-
+from flask.ext.mail import Mail
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -15,6 +15,7 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+mail = Mail(app)
 
 from app import views, models
 
